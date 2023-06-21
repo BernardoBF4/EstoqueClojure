1acbe5aedb851bf38e9d869ee411efe06a4383b5
BernardoBF4 <bernardo.fantin@gmail.com>
2023-06-20 22:33:12 -0300

    feat(funções anônimas): introdução

--- src/estoque/aula3.clj
+++ src/estoque/aula3.clj
@@ -79,14 +79,45 @@
 
 
 
+(println "#######################################")
+
+(defn mais-caro-que-100?
+  [valor-bruto]
+  (> valor-bruto 100))
 
 
+(defn valor-descontado
+  "Retorna o valor com desconto de 10% se deve aplicar desconto."
+  [aplica? valor-bruto]
+  (if (aplica? valor-bruto)
+    (let [taxa-de-desconto (/ 10 100)
+          desconto         (* valor-bruto taxa-de-desconto)]
+      (- valor-bruto desconto))
+    valor-bruto))
+
+(println (valor-descontado mais-caro-que-100? 1000))
+(println (valor-descontado mais-caro-que-100? 100))
+
+; HIGHER ORDER FUNCTIONS
+; IMUTABILIDADE (a exemplos dos vetores)
 
 
+(println "#### Função sem nome (anônima) ###########################")
+(println (valor-descontado (fn [valor-bruto] (> valor-bruto 100)) 1000))
+(println (valor-descontado (fn [valor-bruto] (> valor-bruto 100)) 100))
 
 
+(println "#### Função sem nome (anônima) abreviada ###########################")
+(println (valor-descontado #(> %1 100) 1000))
+(println (valor-descontado #(> %1 100) 100))
 
 
+(println "#### Função sem nome (anônima) abreviada só com um parâmetro ###########################")
+(println (valor-descontado #(> % 100) 1000))
+(println (valor-descontado #(> % 100) 100))
 
+(def mais-caro-que-100? #(> % 100))
+(println (valor-descontado mais-caro-que-100? 1000))
+(println (valor-descontado mais-caro-que-100? 100))
 
 
