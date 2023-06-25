cac9c4bb30d0abfa74402f777bbd9e489c91622e
BernardoBF4 <bernardo.fantin@gmail.com>
2023-06-24 23:34:36 -0300

    feat(thread last): usando thread last com maps e reduce

---
+++ src/estoque/aula6.clj
@@ -0,0 +1,109 @@
+(ns estoque.aula6)
+
+(def pedido
+  {:mochila {:quantidade 2, :preco 80}
+   :camiseta {:quantidade 3, :preco 40}})
+
+(defn imprime-e-15
+  [valor]
+  (println "valor" (class valor) valor)
+  15)
+
+(println (map imprime-e-15 pedido))
+
+
+;(defn imprime-e-15
+;  [chave valor]
+;  (println "valor" valor "e" chave)
+;  15)
+;
+;(println (map imprime-e-15 pedido))
+
+(println "######")
+(defn imprime-e-15
+  [[chave valor]]
+  (println "valor" valor "e chave" chave)
+  15)
+
+(println (map imprime-e-15 pedido))
+
+(defn preco-dos-produto
+  [[chave valor]]
+  (* (:quantidade valor) (:preco valor)))
+
+(println (map preco-dos-produto pedido))
+(println (reduce + (map preco-dos-produto pedido)))
+
+
+
+
+(println "Versão com parâmetro da 'chave' que não era utilziado substituído por um _")
+(defn preco-dos-produto
+  [[_ valor]]
+  (* (:quantidade valor) (:preco valor)))
+
+(println "Total por produto:" (map preco-dos-produto pedido))
+(println "Total do pedido:" (reduce + (map preco-dos-produto pedido)))
+
+(defn total-do-pedido
+  [pedido]
+  (reduce + (map preco-dos-produto pedido)))
+
+(println (total-do-pedido pedido))
+
+
+; THRED LAST: o parâmetro passado pra função vai no final da chamada.
+; Então o (map preco-dos-produto ,,,) vai ter o parâmetro onde tem as vírgulas que eu adicionei;
+; já no thread first, o parâmetro vem pro primeiro: assim -> (map ,,, preco-dos-produto)
+(defn total-do-pedido
+  [pedido]
+  (->> pedido
+      (map preco-dos-produto)
+      (reduce +)))
+
+(println "Total do pedido" (total-do-pedido pedido))
+
+
+
+
+
+(defn preco-total-do-prduto
+  [produto]
+  (println "Produto"produto)
+  (* (:quantidade produto) (:preco produto)))
+
+(defn preco-total-do-pedido
+  [pedido]
+  (->> pedido
+       vals
+       (map preco-total-do-prduto)
+       (reduce +)))
+
+(println "Total do pedido" (preco-total-do-pedido pedido))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
